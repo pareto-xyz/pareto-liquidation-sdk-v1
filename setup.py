786fce32c8b2ec6d1d0fa79f0f3d6e4ccc710141
mhw32 <me@mikewuis.me>
2022-10-05 13:50:34 +0800

    initial_commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open("README.md", "r").read()
+
+setup(
+    name="pareto-liquidator",
+    version="0.1.0",
+    author="grubiroth",
+    author_email="mike@paretolabs.xyz",
+    packages=find_packages(),
+    scripts=["liquidator/bin/main.py"],
+    description="Bot for maintaining Pareto's oracles",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pareto-xyz/pareto-liquidation-bot-v1",
+    install_requires=[
+        requests==2.28.1
+        setuptools==41.2.0
+        web3==5.30.0
+    ]
+)
